9f58c13a6ec745a2b16be44850a4505a7c597684
GrafEnters <graf.enters@gmail.com>
2024-02-24 17:32:08 +0300

    Added reachDistance & fixes

--- Assets/Scripts/WolfCC.cs
+++ Assets/Scripts/WolfCC.cs
@@ -1,10 +1,12 @@
-using System;
 using System.Collections;
-using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.AI;
 
 public class WolfCC : CreatureBase {
+    [SerializeField]
+    private float _reachDistance = 0.75f;
+    
+    
     [SerializeField]
     private FieldOfView _fov;
 
@@ -13,10 +15,11 @@ public class WolfCC : CreatureBase {
 
     private Vector3 _target;
     private GameObject _targetObj;
+    private Coroutine _taskCoroutine;
 
     protected override void Awake() {
         _fov.Init(IsImportant, OnSeeObj, OnRemoveObj);
-        InvokeRepeating(nameof(UpdateTarget), 0.5f, 0.5f);
+        InvokeRepeating(nameof(UpdateTarget), 0.3f, 0.3f);
     }
 
     private bool IsImportant(GameObject obj) {
@@ -34,6 +37,7 @@ public class WolfCC : CreatureBase {
     private void OnSeeObj(GameObject obj) {
         if (obj.tag == "Player") {
             _targetObj = obj;
+            StopTask();
         }
     }
 
@@ -44,7 +48,7 @@ public class WolfCC : CreatureBase {
     }
 
     private void UpdateTarget() {
-        if (_navAgent.isStopped) {
+        if (!_navAgent.enabled) {
             return;
         }
 
@@ -53,22 +57,56 @@ public class WolfCC : CreatureBase {
             _navAgent.SetDestination(_target);
         }
 
-        if ((_targetObj.transform.position - transform.position).magnitude <= 0.5f) {
+        if ((_navAgent.destination - transform.position).magnitude <= _reachDistance) {
             OnReachDestination();
         }
     }
 
     private void OnReachDestination() {
+        if (_targetObj == null) {
+            _taskCoroutine = StartCoroutine(LookAround());
+            return;
+        }
+
         if (_targetObj.CompareTag("Player")) {
             StartCoroutine(Attack());
         }
     }
 
+    private IEnumerator LookAround() {
+      
+        _navAgent.enabled = false;
+        float time = 0;
+        var rb = GetComponent<Rigidbody>();
+        while (time < 1) {
+            Vector3 euler = rb.rotation.eulerAngles;
+            euler.y += 360 * Time.fixedDeltaTime;
+            rb.MoveRotation(Quaternion.Euler(euler));
+            yield return new WaitForFixedUpdate();
+            time += Time.fixedDeltaTime;
+        }
+
+        Debug.Log($"{gameObject.name} looking around!");
+        yield return new WaitForSeconds(1);
+       
+        _navAgent.enabled = true;
+    }
+
     private IEnumerator Attack() {
-        _navAgent.isStopped = true;
+      
+        _navAgent.enabled = false;
         Debug.Log($"{gameObject.name} attacked player!");
         yield return new WaitForSeconds(1);
-        _navAgent.isStopped = false;
+
+        _navAgent.enabled = true;
+    }
+
+    private void StopTask() {
+        if (_taskCoroutine != null) {
+            StopCoroutine(_taskCoroutine);
+        
+            _navAgent.enabled = true;
+        }
     }
 
     private void OnDrawGizmosSelected() {
