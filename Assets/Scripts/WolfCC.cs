04439989278c20782da4400edf05c8686760de54
GrafEnters <graf.enters@gmail.com>
2024-02-24 16:14:00 +0300

    Removed CharacterController

--- Assets/Scripts/WolfCC.cs
+++ Assets/Scripts/WolfCC.cs
@@ -1,11 +1,21 @@
+using System;
+using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class WolfCC : CreatureBase {
     [SerializeField]
     private FieldOfView _fov;
+    
+    [SerializeField]
+    private NavMeshAgent _navAgent;
 
+    private Vector3 _target;
+    private GameObject _targetObj;
+    
     protected override void Awake() {
-        _fov.Init(IsImportant, null, null);
+        _fov.Init(IsImportant, OnSeeObj, OnRemoveObj);
+        InvokeRepeating(nameof(UpdateTarget), 0.5f,0.5f);
     }
 
     private bool IsImportant(GameObject obj) {
@@ -20,4 +30,26 @@ public class WolfCC : CreatureBase {
         return false;
     }
 
+    private void OnSeeObj(GameObject obj) {
+        if (obj.tag == "Player") {
+            _targetObj = obj;
+        }
+    }
+    private void OnRemoveObj(GameObject obj) {
+        if (obj == _targetObj) {
+            _targetObj = null;
+        }
+    }
+
+    private void UpdateTarget() {
+        if (_targetObj != null) {
+            _target = _targetObj.transform.position;
+            _navAgent.SetDestination(_target);
+        }
+    }
+
+    private void OnDrawGizmosSelected() {
+        Gizmos.color = Color.yellow;
+        Gizmos.DrawWireCube(_target, Vector3.one * 0.5f);
+    }
 }
