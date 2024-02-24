ccc0ed1c85b2871f593f812bf45886b2d715a7e4
GrafEnters <graf.enters@gmail.com>
2024-02-24 17:41:24 +0300

    InverseKinematics scripts

---
+++ Assets/Scripts/InverseKinematics/BodyHeight.cs
@@ -0,0 +1,24 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BodyHeight : MonoBehaviour {
+    [SerializeField]
+    private List<FootMover> _targetFootPoints;
+
+    private void Update() {
+        CalculateHeight();
+    }
+
+    public void CalculateHeight() {
+        // Мы определяем новую высоту для тела через среднее значение высоты ног. 
+        float sum = 0f;
+
+        for (int i = 0; i < _targetFootPoints.Count; i++) {
+            sum += _targetFootPoints[i].NewTarget.y; // Для этого нам и понадобилось свойство в FootMover'е
+        }
+
+        float newHeight = sum / _targetFootPoints.Count;
+
+        transform.position = new Vector3(transform.position.x, newHeight, transform.position.z);
+    }
+}
